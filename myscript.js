6541595829e29d88d49a695391e6d57c4f94dba6
krishnakr05 <krishnakr022021@gmail.com>
2025-09-20 21:46:44 +0530

    Added functionality for evaluation of operation

--- myscript.js
+++ myscript.js
@@ -1,6 +1,10 @@
 let num1, num2, operator
-
 let result;
+let firstnum=''
+let secondnum=''
+let currentOp=null;
+let shouldResetDisplay= false
+
 function add(a,b){
     return a+b;
 }
@@ -22,22 +26,73 @@ function operate(n1, n2, operator){
     switch(operator){
         case '+':
             result=add(n1, n2);
-            console.log(result)
-            break;
+            return result;
         case '-':
             result=subtract(n1, n2);
-            console.log(result)
-            break;
+            return result;
         case '*':
             result= multiply(n1, n2);
-            console.log(result)
-            break;
+            return result;
         case '/':
             result=divide(n1, n2);
-            console.log(result)
-            break;
+            return result;
     }
 }
 
 operate(5, 3, '*')
+
+function appendNumber(n){
+    if(display.textContent==='0'|| shouldResetDisplay){
+        display.textContent=''
+        shouldResetDisplay=false
+    }
+    display.textContent+=n
+}
+
+function setOperator(op){
+    if(currentOp!== null) evaluate();
+    firstnum=display.textContent
+    currentOp=op
+    shouldResetDisplay= true
+}
+
+function evaluate(){
+    if (currentOp===null) return;
+    secondnum= display.textContent;
+    let result= operate(parseFloat(firstnum), parseFloat(secondnum), currentOp);
+    display.textContent=result;
+    currentOp= null;
+}
+
+
+
+const display= document.querySelector('.display')
+
+const numberButtons= document.querySelectorAll('.number');
+numberButtons.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        appendNumber(button.textContent)
+    })
+})
+
+const opButtons= document.querySelectorAll('.operator');
+opButtons.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        setOperator(button.textContent)
+    })
+})
+
+const equalsButton= document.querySelectorAll('.equals');
+equalsButton.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        evaluate();
+    })
+})
+
+const clrButton= document.querySelectorAll('.clear');
+clrButton.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        console.log('clr clicked')
+    })
+})
 
